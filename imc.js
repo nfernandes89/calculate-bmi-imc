9dc997b0a635081e4409995ffcc238dd47132515
nfernandes89 <113728338+nfernandes89@users.noreply.github.com>
2022-11-02 23:11:03 +0000

    change img in .html

--- imc.js
+++ imc.js
@@ -14,23 +14,19 @@ function calculateIMC(event) {
   let imcResult = document.querySelector(".result-js");
 
   if (imc < 18.5) {
-    document
-      .querySelector(".img-attention")
-      .setAttribute("src", "attention.png");
+    document.querySelector(".semaforo").setAttribute("src", "attention.png");
     imcResult.setAttribute("class", "result-js");
     imcResult.innerHTML = imc + ": Underweight";
   } else if (imc > 18.5 && imc <= 24.9) {
-    document.querySelector(".img-green").setAttribute("src", "light-green.png");
+    document.querySelector(".semaforo").setAttribute("src", "light-green.png");
     imcResult.setAttribute("class", "result-js");
     imcResult.innerHTML = imc + ": Normal weight";
   } else if (imc > 25 && imc <= 29.9) {
-    document
-      .querySelector(".img-yellow")
-      .setAttribute("src", "light-yellow.png");
+    document.querySelector(".semaforo").setAttribute("src", "light-yellow.png");
     imcResult.setAttribute("class", "result-js");
     imcResult.innerHTML = imc + ": Overweight";
   } else if (imc > 30) {
-    document.querySelector(".img-red").setAttribute("src", "light-red.png");
+    document.querySelector(".semaforo").setAttribute("src", "light-red.png");
     imcResult.setAttribute("class", "result-js");
     imcResult.innerHTML = imc + ": Obesity";
   }
